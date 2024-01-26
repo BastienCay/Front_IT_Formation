242a8c2253a746bb42587f544c947abef6b007e1
Alexis Donnez <alexis.donnez62@gmail.com>
2024-01-26 10:37:22 +0100

    Creation Header[V1]: -ajout nav_bar -modification fichier app.routes.ts

--- IT_Formation/src/app/commons/header/header.component.ts
+++ IT_Formation/src/app/commons/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    imports: [NavBarComponent]
 })
 export class HeaderComponent {
 
