c784154fb1e3480ce31d0918f55d10d29337225d
Bastien <bcay.git@gmail.com>
2024-01-29 22:08:31 +0100

    edit routes

--- IT_Formation/src/app/app.routes.ts
+++ IT_Formation/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PageFormulaireEvaluationComponent } from './pages/page-formulaire-evaluation/page-formulaire-evaluation.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'page-formulaire-evaluation', pathMatch:'full'},
+    {path: '', redirectTo: 'home-page', pathMatch:'full'},
     {path:'home-page', component: HomePageComponent},
     {path:'page-formulaire-evaluation', component: PageFormulaireEvaluationComponent},
 ];
