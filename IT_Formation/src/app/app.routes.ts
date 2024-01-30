33078dcd0f2f5b0f074cef3979bc3f2ad47314e3
Bastien <bcay.git@gmail.com>
2024-01-30 16:18:17 +0100

    css partie 1

--- IT_Formation/src/app/app.routes.ts
+++ IT_Formation/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PageFormulaireEvaluationComponent } from './pages/page-formulaire-evaluation/page-formulaire-evaluation.component';
+import { FormulaireEvaluationComponent } from './pages/page-formulaire-evaluation/formulaire-evaluation/formulaire-evaluation.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home-page', pathMatch:'full'},
     {path:'home-page', component: HomePageComponent},
-    {path:'page-formulaire-evaluation', component: PageFormulaireEvaluationComponent},
+    {path:'evaluation', component: PageFormulaireEvaluationComponent},
+    {path:'eval', component: FormulaireEvaluationComponent},
+    
 ];
