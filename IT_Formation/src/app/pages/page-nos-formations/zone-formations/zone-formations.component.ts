b6a45d06f17d6ce3dbde04718b7524b282376ad5
Alexis Donnez <alexis.donnez62@gmail.com>
2024-02-01 16:15:48 +0100

    card-formation-representation complet, mise en exemple avec un tableau ecris dans le dur

--- IT_Formation/src/app/pages/page-nos-formations/zone-formations/zone-formations.component.ts
+++ IT_Formation/src/app/pages/page-nos-formations/zone-formations/zone-formations.component.ts
@@ -10,12 +10,52 @@ import { CardFormationPresentationComponent } from "./card-formation-presentatio
 })
 export class ZoneFormationsComponent {
 
-    formation: any[] = [{
+    formations: any[] = [
+    {
         titre: "Développeur Full Stack Java",
         reference: "TQT22",
         prix: 2123,
         theme: "Développement java",
-        
-    }];
+        duree: 120,
+        type: "Distanciel / Presentiel",
+        preRequis: 2
+    },
+    {
+        titre: "Développement Java",
+        reference: "DE5TR",
+        prix: 1950,
+        theme: "Développement java",
+        duree: 245,
+        type: "Presentiel",
+        preRequis: 1
+    },
+    {
+        titre: "Développement Python",
+        reference: "EO8D6",
+        prix: 860,
+        theme: "Développement python",
+        duree: 12,
+        type: "Distanciel",
+        preRequis: 1
+    },
+    {
+        titre: "Développement Java",
+        reference: "DE5TR",
+        prix: 1950,
+        theme: "Développement java",
+        duree: 245,
+        type: "Presentiel",
+        preRequis: 1
+    },
+    {
+        titre: "Développement Python",
+        reference: "EO8D6",
+        prix: 860,
+        theme: "Développement python",
+        duree: 12,
+        type: "Distanciel",
+        preRequis: 1
+    }
+    ];
 
 }
