69fed4b1977549bcfb2b63ea356e1a487075eb44
caroline <cagrabarz@gmail.com>
2024-02-05 14:24:24 +0100

    intégration de la loupe Reste à la mettre en saumon + la placer

--- IT_Formation/src/app/commons/search-bar/search-bar.component.ts
+++ IT_Formation/src/app/commons/search-bar/search-bar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatIconModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
+
 export class SearchBarComponent {
   searchBar: String = "";
 }
