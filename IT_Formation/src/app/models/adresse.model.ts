ed99a0b2b54ecb300a6631a8a9b282ef34e85676
Alexis Donnez <alexis.donnez62@gmail.com>
2024-02-06 15:56:19 +0100

    feat: :sparkles: Connection Front Spring

---
+++ IT_Formation/src/app/models/adresse.model.ts
@@ -0,0 +1,7 @@
+export default interface Adresse {
+    id: Number,
+    codePostal: String,
+    ville: String,
+    rue: String,
+    pays: String
+}
---
+++ IT_Formation/src/app/models/sessionFormation.model.ts
@@ -0,0 +1,33 @@
+export default interface SessionFormation {
+    id_session: Number,
+    dateDebut: String,
+    dateFin: String,
+    formation: {
+        id: Number,
+        nom: String,
+        descript: String;
+        prix: Number;
+        reference: String;
+        typeFormation: Number;
+        preRequis: Number;
+        typeCertification: Number,
+        metiers: String,
+        adresse: {
+            id: Number,
+            codePostal: String,
+            ville: String,
+            rue: String,
+            pays: String
+            },
+            sousTheme: {
+                id: Number,
+                designation: String,
+                theme: {
+                    id: Number,
+                    designation: String
+                    }
+        }
+    },
+    "formateur": null,
+    "user": null
+}
