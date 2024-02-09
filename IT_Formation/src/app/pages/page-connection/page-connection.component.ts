9e588dae7b1230c0295cf6f96b20008eed777bc2
caroline <cagrabarz@gmail.com>
2024-02-09 12:29:56 +0100

    hotfix titre page connexion

--- IT_Formation/src/app/pages/page-connection/page-connection.component.ts
+++ IT_Formation/src/app/pages/page-connection/page-connection.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../commons/header/header.component";
 import { FooterComponent } from "../../commons/footer/footer.component";
 import { FormulaireConnectionComponent } from "./formulaire-connection/formulaire-connection.component";
+import { SaumongComponent } from "../../commons/saumong/saumong.component";
 
 @Component({
     selector: 'app-page-connection',
     standalone: true,
     templateUrl: './page-connection.component.html',
     styleUrl: './page-connection.component.css',
-    imports: [HeaderComponent, FooterComponent, FormulaireConnectionComponent]
+    imports: [HeaderComponent, FooterComponent, FormulaireConnectionComponent, SaumongComponent]
 })
 export class PageConnectionComponent {
 
